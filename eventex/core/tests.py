d1d20ffda7665432d43c17ca4bc0b6a21e6eff41
Ederson Santos <edersonramalho@gmail.com>
2017-04-17 18:22:12 -0300

    Adiciona teste na home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+"""
+2017/04/17
+aula M2A10: O resgate do código não testado
+Ederson
+"""
+
+#Classe é um senário de teste, pois herda o TestCase
+class HomeTest(TestCase):
+    
+    def setUp(self):
+        """  """
+        
+        #Criar uma variável na class(self)
+        self.response = self.client.get('/')
+    
+    def test_get(self):
+        """GET / must return status code 200 """
+        self.assertEqual(200,self.response.status_code)
+        
+    def test_template(self):
+        """Must use  index.html"""
+        #response = self.client.get('/')
+        self.assertTemplateUsed(self.response, 'index.html')
